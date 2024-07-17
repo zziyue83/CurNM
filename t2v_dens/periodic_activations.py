63ba1a5cf8e3b6eed2dcece31dc6ace523e50767
ZC <45113315+zziyue83@users.noreply.github.com>
2024-07-17 11:48:14 +0800

    Add files via upload

---
+++ t2v_dens/periodic_activations.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn
+
+def t2v(tau, f, out_features, w, b, w0, b0, arg=None):
+    if arg:
+        v1 = f(torch.matmul(tau, w) + b, arg)
+    else:
+        #print(w.shape, t1.shape, b.shape)
+        v1 = f(torch.matmul(tau, w) + b)
+    v2 = torch.matmul(tau, w0) + b0
+    #print(v1.shape)
+    return torch.cat([v1, v2], -1)
+
+class SineActivation(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(SineActivation, self).__init__()
+        self.out_features = out_features
+        self.w0 = nn.parameter.Parameter(torch.randn(in_features, 1))
+        self.b0 = nn.parameter.Parameter(torch.randn(1))
+        self.w = nn.parameter.Parameter(torch.randn(in_features, out_features-1))
+        self.b = nn.parameter.Parameter(torch.randn(out_features-1))
+        self.f = torch.sin
+
+    def forward(self, tau):
+        return t2v(tau, self.f, self.out_features, self.w, self.b, self.w0, self.b0)
+
+class CosineActivation(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(CosineActivation, self).__init__()
+        self.out_features = out_features
+        self.w0 = nn.parameter.Parameter(torch.randn(in_features, 1))
+        self.b0 = nn.parameter.Parameter(torch.randn(1))
+        self.w = nn.parameter.Parameter(torch.randn(in_features, out_features-1))
+        self.b = nn.parameter.Parameter(torch.randn(out_features-1))
+        self.f = torch.cos
+
+    def forward(self, tau):
+        return t2v(tau, self.f, self.out_features, self.w, self.b, self.w0, self.b0)
+
+class Time2Vec(nn.Module):
+    def __init__(self, activation, hiddem_dim, output_dim):
+        super(Time2Vec, self).__init__()
+        if activation == "sin":
+            self.l1 = SineActivation(1, hiddem_dim)
+        elif activation == "cos":
+            self.l1 = CosineActivation(1, hiddem_dim)
+        
+        self.fc1 = nn.Linear(hiddem_dim, output_dim)
+    
+    def forward(self, x):
+        #x = x.unsqueeze(1)
+        x = self.l1(x)
+        x = self.fc1(x)
+        return x
